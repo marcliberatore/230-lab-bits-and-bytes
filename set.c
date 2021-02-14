e0e83de2842a63ad6453fcf0636312b92f01a680
Qingyang Xue <xqy@Qingyangs-Air.hsd1.ma.comcast.net>
2021-02-14 15:20:38 -0500

    .

--- set.c
+++ set.c
@@ -1,13 +1,24 @@
 #include "set.h"
 
+// Add an element with index v to the set.
+// Returns -1 if v is negative or beyond the maximum capacity of the set.
+// Returns 0 if v is already in the set.
+// Returns 1 if v is successfully added.
 static int add_element(int v) {
   return 0;
 }
 
+// Remove an element with index v from the set.
+// Returns -1 if v is negative or beyond the maximum capacity of the set.
+// Returns 0 if v is already not in the set.
+// Returns 1 if v is successfully removed.
 static int remove_element(int v) {
   return 0;
 }
 
+// Checks if an element with index v is contained in the set.
+// Returns 0 if v is not in the set.
+// Returns 1 if v is in the set.
 static int contains_element(int v) {
   return 0;
 }
