d3d3c493aa4f3d026658718ef9978711789e337b
Marc Liberatore <liberato@cs.umass.edu>
2022-09-22 07:02:02 -0400

    drink coffee, get rid of strcat()

--- set.c
+++ set.c
@@ -7,15 +7,12 @@
 // or you'd need to pass in a pointer to a buffer you'd already allocated.
 // But here, you can just leak the memory.
 char* as_binary(unsigned int n) {
-  char *b = (char *)malloc(33);
-  b[0] = '\0';
-
-  unsigned int z;
-  for (z = 1U << 31; z > 0; z >>= 1) {
-    strcat(b, ((n & z) == z) ? "1" : "0");
-  }
-
-  return b;
+    char* s = malloc(33);
+    s[32] = '\0';
+    for (int i = 0; i < 32; i++) {
+        s[31-i] = (n & (1 << i)) ? '1' : '0';
+    }
+    return s;
 }
 
 // Add an element with index v to the set I.
