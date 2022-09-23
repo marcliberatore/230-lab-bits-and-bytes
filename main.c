3298a72995b5493671d86c18b9257db9ebf6ec24
Marc Liberatore <liberato@cs.umass.edu>
2022-09-23 06:40:42 -0400

    Update main.c

--- main.c
+++ main.c
@@ -3,18 +3,18 @@
 #include <string.h>
 
 int main() {
+  I = 0;
   memset(J, 0, sizeof(J)); // set every bit in the array to 0
 
   // Write your tests here. For example:
-  //part1_add_element(1);
-  //part1_add_element(24);
-  //part1_add_element(51);
-  //part2_add_element(66);
-  //part2_add_element(-21);
-  //etc.
+  // part1_add_element(0);
+  // part1_add_element(2);
+
 
   // an example use of as_binary(); feel free to comment out or change
   // this to help you in your work!
+  printf("I: %s %d\n", as_binary(I), I);
+
   for (int i = 0; i < N; i++) {
     printf("%d: %s %d\n", i, as_binary(J[i]), J[i]);
   }
